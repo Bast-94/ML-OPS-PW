790ab8236f02a5a6f2fea1a077e00252d98b41d6
Bast-94 <bh.69@live.fr>
2023-09-29 17:19:32 +0200

    ADD(config-manager.py): Ajout de fonction de gestion de configuration.

---
+++ src/config-manager.py
@@ -0,0 +1,14 @@
+import yaml
+def save_config(dictionnaire, chemin_fichier="config.yml"):
+    with open(chemin_fichier, "w") as fichier:
+        yaml.dump(dictionnaire, fichier)
+    print("Fichier YAML sauvegardé avec succès.")
+
+
+def read_yaml(chemin_fichier):
+    if os.path.exists(chemin_fichier):
+        with open(chemin_fichier, "r") as fichier:
+            contenu = yaml.safe_load(fichier)
+    else:
+        contenu = {}
+    return contenu
