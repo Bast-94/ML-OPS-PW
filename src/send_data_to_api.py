71d39dcb324bd6299011ae34da1721921dcd32d3
Bast-94 <bh.69@live.fr>
2023-09-29 17:25:07 +0200

    ADD(src/send_data_to_api.py).

--- src/send_data_to_api.py
+++ src/send_data_to_api.py
@@ -1,6 +1,6 @@
 import requests
 
 payload = {"X": [[101, 3]]}
-url = "http://localhost:8000/predict"
+url = "http://localhost:5000/predict"
 response = requests.post(url, json=payload)
 print(response.json())
